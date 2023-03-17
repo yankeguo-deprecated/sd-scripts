295d26d3c03f12634c96330bfb427e272edb7c5f
Guo Y.K <hi@guoyk.net>
2023-03-17 10:05:01 +0800

    add diffusers-model-to-checkpoint.py

---
+++ models/diffusers-model-to-checkpoint.py
@@ -0,0 +1,24 @@
+import os
+import re
+import sys
+
+import torch
+
+from diffusers import StableDiffusionPipeline
+
+model_id = sys.argv[1]
+
+output = re.sub(r'[^A-Za-z0-9]+', '-', model_id) + ".safetensors"
+
+model, cached_dir = StableDiffusionPipeline.from_pretrained(
+    sys.argv[1],
+    torch_dtype=torch.float16,
+    resume_download=True,
+    return_cached_folder=True,
+)
+
+os.system(
+    f'python ../../diffusers/scripts/convert_diffusers_to_original_stable_diffusion.py '
+    f'--model_path "{cached_dir}" '
+    f'--checkpoint_path "{output}"'
+    f'--use_safetensors')
